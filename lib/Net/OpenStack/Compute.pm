8dccf72c73b15c3d4b5122016bc0e258cab92c67
curtis <curtis@oscompute.novalocal>
2013-10-15 21:21:21 +0000

    have oscompute returning live_image list <serverid>

--- lib/Net/OpenStack/Compute.pm
+++ lib/Net/OpenStack/Compute.pm
@@ -7,6 +7,7 @@ use Carp;
 use HTTP::Request;
 use JSON qw(from_json to_json);
 use LWP;
+use Data::Dumper;
 
 has auth_url     => (is => 'rw', required => 1);
 has user         => (is => 'ro', required => 1);
@@ -202,6 +203,88 @@ sub get_flavor {
     return undef unless $res->is_success;
     return from_json($res->content)->{flavor};
 }
+
+## VMS actions
+
+# Launch a clone from a live image.
+#
+# Allowed params:
+#
+# 'name': Name of new server
+# 'guest': Hash of guest parameters
+# 'security_groups': List of security groups
+# 'availability_zone': Nova availability zone
+# 'scheduler_hints': Scheduler hints
+# 'num_instances': Number of instances to launch
+# 'key_name': Name of keypair
+# 'user_data': base64 encoded user data
+sub live_image_start {
+    my ($self, $live_image, $data) = @_;
+    croak "live image id is required" unless $live_image;
+    croak "invalid data" unless $data and 'HASH' eq ref $data;
+    my $res = $self->_action($live_image, gc_launch => $data);
+    return from_json($res->content)->{servers};
+}
+
+# Create a new live image from a server.
+#
+# Allowed params:
+#
+# 'name': Name of live image. If not given, a
+#         name will be auto-generated from the
+#         name of the server.
+sub live_image_create {
+    my ($self, $server , $data) = @_;
+    croak "server id is required" unless $server;
+    croak "invalid data" unless $data and 'HASH' eq ref $data;
+    my $res = $self->_action($server, gc_bless => $data);
+    return from_json($res->content)->{servers};
+}
+
+# Discard a live image
+#
+sub live_image_delete {
+    my ($self, $live_image) = @_;
+    croak "live image id is required" unless $live_image;
+    my $res = $self->_action($live_image, gc_discard => {});
+    return _check_res($res);
+}
+
+# Migrate a VM, using VMS
+#
+# Allowed params:
+#
+# 'dest': Name of host to migrate to, as reported
+#         by nova host-list.
+sub live_migrate {
+    my ($self, $live_image, $data) = @_;
+    croak "live image id is required" unless $live_image;
+    croak "invalid data" unless $data and 'HASH' eq ref $data;
+    my $res = $self->_action($live_image, gc_migrate => $data);
+    return _check_res($res);
+}
+
+# List servers launched from a live image
+#
+sub list_launched {
+    my ($self, $live_image) = @_;
+    croak "live image id is required" unless $live_image;
+    my $res = $self->_action($live_image, gc_list_launched => {});
+    return from_json($res->content)->{servers};
+}
+
+# List live images for a server
+#
+sub list_blessed {
+    # XXX FIXME
+    my ($self, $arg, $id) = @_;
+    croak "list_blessed server id is required" unless $id;
+    my $res = $self->_action($id, gc_list_blessed => {});
+    #return from_json($res->content)->{status};
+    return from_json($res->content);
+}
+
+## End VMS actions
 
 sub _url {
     my ($self, $path, $is_detail, $query) = @_;
